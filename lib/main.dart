8e68fbe0a1b25645ac3d9f397093e65a83b7a5c5
김형주 <joo6077@gmail.com>
2021-03-01 03:09:54 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print('hi');
+}
+
+class FirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
